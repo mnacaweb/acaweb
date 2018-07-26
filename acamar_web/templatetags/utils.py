daf51d9046d8304a16806241df43e759569b7225
Jiří Makarius <jiri.makarius@proboston.net>
2018-07-26 15:54:00 +0200

    money formatter

--- acamar_web/templatetags/utils.py
+++ acamar_web/templatetags/utils.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 
 import base64
 import json
+import re
 
 from django import template
 from django.template.defaultfilters import stringfilter
@@ -23,3 +24,13 @@ def email_link(value):
         mail = value.split("@")
         return "aca{}mar@{}".format(base64.b64encode(mail[0]), mail[1])
     return value
+
+
+@register.filter(name="money")
+@stringfilter
+def money(value):
+    new = re.sub("^(-?\d+)(\d{3})", '\g<1> \g<2>', value)
+    if value == new:
+        return "{} Kč".format(value)
+    else:
+        return money(new)
