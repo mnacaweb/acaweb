2c1ef6744e06aee01daeca9cffe0f60cd96feb9a
Lukáš Sova <lukas@proboston.net>
2019-07-01 12:50:32 +0200

    debug

--- acamar_web/settings.py
+++ acamar_web/settings.py
@@ -69,7 +69,7 @@ elif DEV_PROFILE == "preview":
 
 elif DEV_PROFILE == "master":
     FILER_DEBUG = False
-    DEBUG = True
+    DEBUG = False
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.mysql",
